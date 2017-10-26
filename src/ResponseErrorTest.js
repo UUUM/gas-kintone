dd7604c9509b0657a6e84d7925f900d7c66dedcb
Masato Bito <masato@bz2.jp>
2017-10-26 15:57:36 +0900

    use TestCommon

--- src/ResponseErrorTest.js
+++ src/ResponseErrorTest.js
@@ -1,22 +1,16 @@
-testRunner.functions.push(function (test) {
-  var error;
-  var response;
-
-  function setup() {
-    var client = (new TestCommon()).getClient();
-    response = client.fetchGet('record');
-    error = new ResponseError(response);
-  }
-
+testRunner.functions.push(function (test, common) {
   test('new ResponseError()', function (assert) {
-    setup();
+    var client = common.getClient();
 
-    error = new ResponseError(response);
+    var response = client.fetchGet('record');
+    var content = response.getContentObject();
+
+    var error = new ResponseError(response);
     assert.ok(error instanceof ResponseError, 'creates ResponseError object with a valid argument');
     assert.equal(error.response, response, 'has a response property');
-    assert.equal(error.id, response.getContentObject().id, 'has an id property');
-    assert.equal(error.code, response.getContentObject().code, 'has a code property');
-    assert.equal(error.message, response.getContentObject().message, 'has a message property');
+    assert.equal(error.id, content.id, 'has an id property');
+    assert.equal(error.code, content.code, 'has a code property');
+    assert.equal(error.message, content.message, 'has a message property');
   });
 });
 
