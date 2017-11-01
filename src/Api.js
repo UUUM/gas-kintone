9b7c19cfa2ebbc11ae34a3a6ef5626c8e6f8b3fa
Masato Bito <masato@bz2.jp>
2017-11-01 16:21:17 +0900

    add fetch()

--- src/Api.js
+++ src/Api.js
@@ -7,11 +7,33 @@ var Api = function Api(client) {
   this.lastResponse = null;
 };
 
-Api.prototype.formGet = function formGet() {
-  var response = this.lastResponse = this.client.fetchGet('form');
+Api.prototype.fetch = function fetch(method, command, params, callback) {
+  var response = this.lastResponse = this.client[method].call(this.client, command, params);
   if (response.getResponseCode() !== 200) {
     return new ResponseError(response);
   }
 
-  return response.getContentObject().properties;
+  return callback.call(this, response);
+};
+
+Api.prototype.fetchDelete = function fetchDelete(command, params, callback) {
+  return this.fetch('fetchDelete', command, params, callback);
+};
+
+Api.prototype.fetchGet = function fetchGet(command, params, callback) {
+  return this.fetch('fetchGet', command, params, callback);
+};
+
+Api.prototype.fetchPost = function fetchPost(command, params, callback) {
+  return this.fetch('fetchPost', command, params, callback);
+};
+
+Api.prototype.fetchPut = function fetchPut(command, params, callback) {
+  return this.fetch('fetchPut', command, params, callback);
+};
+
+Api.prototype.formGet = function formGet() {
+  return this.fetchGet('form', {}, function f(response) {
+    return response.getContentObject().properties;
+  });
 };
